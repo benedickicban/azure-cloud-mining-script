131e448e0a6246d399befdfea70cc7202e7e4b91
benedickicban <benedickicban@gmail.com>
2018-05-05 10:52:49 +0800

    Update setup_vm2.sh
    change to sumokoin

--- azure_script/setup_vm2.sh
+++ azure_script/setup_vm2.sh
@@ -2,8 +2,9 @@
 sudo apt-get -y update       
 sudo apt-get -y upgrade  
 sudo apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev leafpad git xauth
-git clone https://github.com/azurecloudminingscript/azure-cloud-mining-script
+git clone https://github.com/benedickicban/azure-cloud-mining-script
 
+git checkout sumokoin
 cd azure-cloud-mining-script
 azure_script/compile_and_config.sh
 
